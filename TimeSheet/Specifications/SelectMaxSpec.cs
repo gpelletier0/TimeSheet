a72f48cafaba803e4e575cc9be029111b119ab12
gpelletier0 <gpelletier0@gmail.com>
2025-10-17 15:07:57 -0400

    feat: add SelectMaxSpec for generating MAX queries with dynamic filtering.

---
+++ TimeSheet/Specifications/SelectMaxSpec.cs
@@ -0,0 +1,30 @@
+﻿using TimeSheet.Interfaces;
+
+namespace TimeSheet.Specifications;
+
+public class SelectMaxSpec : ISpecification {
+    public required string ColumnName { get; init; }
+    public required int Start { get; init; }
+    public int? Length { get; init; }
+    public required SqlDataType DataType { get; init; }
+    public required int Increment { get; init; }
+    public required string TableName { get; init; }
+    public required string Pattern { get; init; }
+
+    public SqlQuery GetQuery() {
+        var subStr = Length.HasValue 
+            ? SqlFuncs.Substr(ColumnName, Start, Length.Value) 
+            : SqlFuncs.Substr(ColumnName, Start);
+        
+        var builder = new SqlQueryBuilder()
+            .SelectMax(SqlFuncs.Add(SqlFuncs.Cast(subStr, DataType), Increment))
+            .From(TableName)
+            .WhereLike(ColumnName, Pattern);
+            
+        return builder.ToQuery();
+    }
+
+    public string GetFilterNames() {
+        return string.Empty;
+    }
+}
