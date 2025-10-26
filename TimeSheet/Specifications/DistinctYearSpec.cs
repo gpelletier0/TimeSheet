e8cdd0a2517eca1ae451abf1403f156db1ccefb9
gpelletier0 <gpelletier0@gmail.com>
2025-10-26 18:01:14 -0400

    refactor: rename `FormatSpecifier` to `Format` in DistinctYearSpec and update query usage

--- TimeSheet/Specifications/DistinctYearSpec.cs
+++ TimeSheet/Specifications/DistinctYearSpec.cs
@@ -6,11 +6,11 @@ public class DistinctTimeSpec : ISpecification {
 
     public required string ColumnName { get; init; }
     public required string TableName { get; init; }
-    public required string FormatSpecifier { get; init; }
+    public required string Format { get; init; }
 
     public SqlQuery GetQuery() {
         var builder = new SqlQueryBuilder()
-            .Select($"DISTINCT strftime('%{FormatSpecifier}', {ColumnName})")
+            .Select($"DISTINCT strftime('{Format}', {ColumnName})")
             .From(TableName);
 
         return builder.ToQuery();
