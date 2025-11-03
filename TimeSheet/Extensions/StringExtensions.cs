ede7aa701a8b373e992d4e64e2a0332d937853a1
gpelletier0 <gpelletier0@gmail.com>
2025-11-03 13:10:26 -0500

    feat: add `JsonDeserialize` extension method for string JSON deserialization.

---
+++ TimeSheet/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+
+namespace TimeSheet.Extensions;
+
+public static class StringExtensions {
+
+    public static T? JsonDeserialize<T>(this string json) {
+        try {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+        catch (JsonException e) {
+            Debug.WriteLine($"Error deserializing JSON: {e}");
+            return default;
+        }
+    }
+}
