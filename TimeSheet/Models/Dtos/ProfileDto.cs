ca040fd28588c069c260cca84219f806b515f2ed
gpelletier0 <gpelletier0@gmail.com>
2025-11-15 13:40:08 -0500

    feat: add `ProfileDto` model for user profile management

---
+++ TimeSheet/Models/Dtos/ProfileDto.cs
@@ -0,0 +1,18 @@
+﻿namespace TimeSheet.Models.Dtos;
+
+public class ProfileDto : BaseDto {
+
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string? Address { get; set; }
+    public string? Address2 { get; set; }
+    public string? City { get; set; }
+    public string? Province { get; set; }
+    public string? Country { get; set; }
+    public string? PostalCode { get; set; }
+    public string Phone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? WebSite { get; set; }
+    public byte[]? Image { get; set; }
+
+}
